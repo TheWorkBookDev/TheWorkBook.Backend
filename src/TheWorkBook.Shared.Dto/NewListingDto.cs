e8fc9b796dfaaf5adf5700035d671461322c2ae1
Ronan Farrell <ronan.farrell@j2.com>
2022-05-02 21:36:03 +0100

    Remove ListingId from NewListingDto.

--- src/TheWorkBook.Shared.Dto/NewListingDto.cs
+++ src/TheWorkBook.Shared.Dto/NewListingDto.cs
@@ -4,7 +4,6 @@
     {
         public decimal Budget { get; set; }
         public int CategoryId { get; set; }
-        public int ListingId { get; set; }
         public int LocationId { get; set; }
         public string MainDescription { get; set; }
         public byte StatusId { get; set; }
